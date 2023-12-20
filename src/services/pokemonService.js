0e50f184ecf638739a783a5d7db71ddf0857e980
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2023-12-20 10:47:38 +0100

    update compose.yml and services

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -2,10 +2,10 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
+  user: process.env.POSTGRES_USER || "postgres",
   host: 'localhost',
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB ||"postgres",
+  password: process.env.POSTGRES_PASSWORD || "postgres",
   port: process.env.POSTGRES_PORT || 5432,
 });
 
