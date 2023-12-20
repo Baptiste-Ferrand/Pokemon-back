68754ef8b0c4d9896d880f266f909fc0550b1455
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2023-12-20 12:11:22 +0100

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const pokemonRoutes = require('./routes/pokemonRoutes');
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use('/pokemon/', pokemonRoutes);
